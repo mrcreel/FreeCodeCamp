d729d79defbe5c45a6bbb909394201f06469adfb
mrcreel <michaelrcreel@gmail.com>
2017-09-28 18:54:34 -0500

    Initial commit

--- 01_Front End Development Certification/10_Intermediate Algorithm Scripting/03_Roman Numeral Converter.js
+++ 01_Front End Development Certification/10_Intermediate Algorithm Scripting/03_Roman Numeral Converter.js
@@ -1,45 +1,42 @@
 function convertToRoman(num) {
 
-	var roman="";
-	var key = "";
-	var val = 0;
-	var max = 0;
+    var roman = "";
 
-	var i = 0;
+    var i = 0;
 
-	var quotient = 0;
-	var remainder = num;
+    var quotient = 0;
+    var remainder = num;
 
-	numerals = {
-		"M":1000,
-		"D":500,
-		"C":100,
-		"L":50,
-		"X":10,
-		"V":5,
-		"I":1
-	};
+    var numerals = [
+    	{"letter":"M",
+    	"value": 1000,
+    	"max" :3},
+    	{"letter":"D",
+    	"value": 500,
+    	"max" :1},
+    	{"letter":"C",
+    	"value": 100,
+    	"max" :3},
+    	{"letter":"L",
+    	"value": 50,
+    	"max" :1},
+    	{"letter":"X",
+    	"value": 10,
+    	"max" :3},
+    	{"letter":"V",
+    	"value": 5,
+    	"max" :1},
+    	{"letter":"I",
+    	"value": 1,
+    	"max" :3}
+    ];
 
-	for (i in numerals){
-    if (numerals.hasOwnProperty(i)) {
-    	
-    	quotient = Math.floor(remainder/numerals[i]);
-    	remainder = num % numerals[i];
+    for(i = 0; i < numerals.length; i++ ){
 
-    	console.log("Key is " + i + ", value is " + numerals[i]);
-    	console.log(quotient, remainder, i);
-    	
+    	quotient = Math.floor(remainder/numerals[i].value);
+    	remainder = remainder%numerals[i].value;
+    	console.log(numerals[i].letter, quotient);
     }
-	}
-
-	// for (key in numerals) {
-	// 	//console.log(numerals[key]);
-	// }
-
-	// for (val in numerals) {
-	// 	// console.log(val);
-	// }
-
 }
 
 convertToRoman(3999);
