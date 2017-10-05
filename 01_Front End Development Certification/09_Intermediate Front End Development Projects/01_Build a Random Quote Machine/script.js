d9357284313d07408701101bc1e192b0b00134ac
mrcreel <michaelrcreel@gmail.com>
2017-10-05 08:20:26 -0500

    Begin work on twitter button

--- 01_Front End Development Certification/09_Intermediate Front End Development Projects/01_Build a Random Quote Machine/script.js
+++ 01_Front End Development Certification/09_Intermediate Front End Development Projects/01_Build a Random Quote Machine/script.js
@@ -1,3 +1,60 @@
-$( document ).ready(function() {
-	
+var varQuote;
+var varAuthor;
+var varTweetLength;
+var varRand;
+
+var varUrl = "https://talaikis.com/api/quotes/";
+
+function funcGetNextQuote(arrQuotes) {
+
+    if (arrQuotes.length > 0) {
+
+        varRand = Math.floor(arrQuotes.length * Math.random());
+
+        varQuote = arrQuotes[varRand].quote;
+        varAuthor = arrQuotes[varRand].author;
+
+        varTweetLength = varQuote.length + varAuthor.length + 3;
+
+        $("#id-quote").html(varQuote);
+        $("#id-author").html(varAuthor);
+
+        if(varTweetLength > 140) {
+        	console.log(varTweetLength);
+        } else {
+        	
+        }
+
+        return arrQuotes;
+
+    } else {
+        funcGetQuoteList();
+    }
+}
+
+function funcGetQuoteList() {
+
+
+    // Get quotes 100 at a time
+    $.getJSON(varUrl, function(data) {
+
+        // Find quotes shorter than 150 chrs
+        arrQuotes = data.filter(function(val) {
+            return (val.quote.length < 140);
+        });
+
+        funcGetNextQuote(arrQuotes);
+
+        $("#btn-next").click(function() {
+            funcGetNextQuote(arrQuotes);
+
+
+        });
+
+    });
+}
+$(document).ready(function() {
+
+	funcGetQuoteList();
+
 });
