b3b13136b4c433ed0c1c9163aed4178b4176c572
mrcreel <michaelrcreel@gmail.com>
2017-10-07 21:58:12 -0500

    It wasn't broke, so I made it wores

--- 01_Front End Development Certification/09_Intermediate Front End Development Projects/01_Build a Random Quote Machine/script.js
+++ 01_Front End Development Certification/09_Intermediate Front End Development Projects/01_Build a Random Quote Machine/script.js
@@ -1,68 +1,30 @@
-var varQuote;
-var varAuthor;
-var varTweetLength;
-var varRand;
-var arrQuotes = [];
-
+//URL of the quote API
 var varUrl = "https://talaikis.com/api/quotes/";
+var i = 0;
 
-function funcGetNextQuote(arrQuotes) {
-	
+// function funcGetQuoteList() {
+//     // Get quotes 100 at a time
+//     $.getJSON(varUrl, function(data) {
+//         /*
+//         funcPopulateQuoteBox(data);
+//         */
+//     });
+// }
 
-    if (arrQuotes.length > 0) {
-
-        varRand = Math.floor(arrQuotes.length * Math.random());
-
-        console.log(varRand);
-
-        varQuote = arrQuotes[varRand].quote;
-        varAuthor = arrQuotes[varRand].author;
-
-        varTweetLength = varQuote.length + varAuthor.length + 3;
-
-        $("#id-quote").html(varQuote);
-        $("#id-author").html(varAuthor);
-
-        if(varTweetLength > 140) {
-        	console.log(varTweetLength);
-        } else {
-        	
-        }
-
-        return arrQuotes;
-
-    } else {
-        funcGetQuoteList();
-    }
-}
-
-function funcGetQuoteList() {
-
-
-    // Get quotes 100 at a time
-    $.getJSON(varUrl, function(data) {
-
-        // Find quotes shorter than 150 chrs
-        arrQuotes = data.filter(function(val) {
-            return (val.quote.length < 140);
-        });
-
-        arrQuotes =  arrQuotes.sort(function() {
-         return 0.5 - Math.random();
-       });
-
-        funcGetNextQuote(arrQuotes);
-
-        $("#btn-next").click(function() {
-            funcGetNextQuote(arrQuotes);
-
-
-        });
-
-    });
-}
 $(document).ready(function() {
 
-	funcGetQuoteList();
+	console.log(i);
+
+	$('#btn-next').click(function() {
+		 i+=1;
+
+		 console.log(i);
+		});
+
+	$('#btn-prev').click(function() {
+		i-=1;
+
+		console.log(i);
+		});
 
 });
