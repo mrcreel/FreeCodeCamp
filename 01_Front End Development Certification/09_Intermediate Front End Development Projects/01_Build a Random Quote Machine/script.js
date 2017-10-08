5984af41b58b16f749efb40383dbee38f5a95cfc
mrcreel <michaelrcreel@gmail.com>
2017-10-08 06:31:23 -0500

    Fixed forward/next buttons

--- 01_Front End Development Certification/09_Intermediate Front End Development Projects/01_Build a Random Quote Machine/script.js
+++ 01_Front End Development Certification/09_Intermediate Front End Development Projects/01_Build a Random Quote Machine/script.js
@@ -1,30 +1,47 @@
 //URL of the quote API
 var varUrl = "https://talaikis.com/api/quotes/";
+//Array of quotes
+var arrQuotes = [];
+//Counter
 var i = 0;
 
-// function funcGetQuoteList() {
-//     // Get quotes 100 at a time
-//     $.getJSON(varUrl, function(data) {
-//         /*
-//         funcPopulateQuoteBox(data);
-//         */
-//     });
-// }
+function funcGetQuoteList() {
+	i = 0;
+	// Get quotes 100 at a time
+  $.getJSON(varUrl, function(arrQuotes) {
+  	$("#id-counter").html(i);
+    funcButtonClicks(arrQuotes);
+  });
+}
+
+function funcButtonClicks(arrQuotes){
+  //console.log(arrQuotes);
+
+  $('#btn-next').click(function() {
+		 i+=1;
+		 $("#id-counter").html(i);
+
+		 if (i === arrQuotes.length){
+		 	funcGetQuoteList();
+		 }
+		});
+
+  $('#btn-prev').click(function() {
+		i-=1;
+		if(i === -1){
+			i = 0;
+			}
+		$("#id-counter").html(i);
+		});
+
+}
+
+function funcPopulateQuoteBox(arrQuotes, i){
+	
+}
 
 $(document).ready(function() {
 
-	console.log(i);
-
-	$('#btn-next').click(function() {
-		 i+=1;
-
-		 console.log(i);
-		});
-
-	$('#btn-prev').click(function() {
-		i-=1;
-
-		console.log(i);
-		});
+	funcGetQuoteList();
 
 });
