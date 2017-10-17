7d94518e037069f84d0f8fa18dc0397f3884bb61
mrcreel <michaelrcreel@gmail.com>
2017-10-17 16:58:48 -0500

    Through Challenge 13

--- 01_Front End Development Certification/10_Intermediate Algorithm Scripting/13_sumFibs.js
+++ 01_Front End Development Certification/10_Intermediate Algorithm Scripting/13_sumFibs.js
@@ -1,11 +1,23 @@
 function sumFibs(num) {
+  var numFib = 0;
+  var arrOddFibs = [];
+  var arrFibs = [1, 1];
 
-
-
-
-
-	
+  function getSum(total, next) {
+    return total + next;
+  }
+  for (var i = 2; i <= num; i++) {
+    numFib = arrFibs[i - 1] + arrFibs[i - 2];
+    if (numFib <= num) {
+      arrFibs.push(numFib);
+    } else {
+      break;
+    }
+  }
+  arrOddFibs = arrFibs.filter(function(el) {
+    return (el % 2 === 1);
+  });
+  num = arrOddFibs.reduce(getSum, 0);
   return num;
 }
-
-sumFibs(4);
+sumFibs(10);
